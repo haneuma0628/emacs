075f6382f753cf387d7ae0069d53411edd24650b
manatee <haneuma0628@gmail.com>
2016-06-09 17:25:45 +0900

    texテンプレートの更新

--- conf/template.el
+++ conf/template.el
@@ -2,6 +2,7 @@
 (auto-insert-mode)
 (setq auto-insert-directory "~/.emacs.d/insert/")
 
-(define-auto-insert "\\.tex$" "tex-template.tex") ; tex
+;; (define-auto-insert "\\.tex$" "tex-short-template.tex") ; tex
+(define-auto-insert "\\.tex$" "tex-standard-template.tex") ; tex
 ;; (define-auto-insert "\\.html$" "html-template.html") ; htmlr
 
