b29f40ed716e2540b3ee7eb2ec23a0057be3ea2e
hnmx4 <haneuma0628@gmail.com>
2016-07-04 13:01:03 +0900

    add key-bind of moving to begin and end of buffer

--- conf/function.el
+++ conf/function.el
@@ -4,6 +4,8 @@
 (bind-key "C-;" 'comment-or-uncomment-region)
 (bind-key "C-t" 'other-window)
 (bind-key "C-c a" 'align)
+(bind-key "C-<" 'beginning-of-buffer)
+(bind-key "C->" 'end-of-buffer)
 (bind-key "ESC ESC ESC" nil) ; 分割破壊阻止
 (bind-key "\C-x \C-c" nil) ; C-x C-cでemacsをkillしない
 (defalias 'exit 'save-buffers-kill-emacs) ; M-x exitでemacsをkill
