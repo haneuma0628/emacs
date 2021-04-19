adefce6b638ad5599ac31feac2edbb101a5b1816
hnmx4 <hnmnty@gmail.com>
2021-04-20 06:28:18 +0900

    Use theme colors for company-mode. And change the way of installation of company-mode to the correct one.

--- conf/conf-company.el
+++ conf/conf-company.el
@@ -7,28 +7,20 @@
   :bind (:map company-active-map
               ("C-n" . company-select-next)
               ("C-p" . company-select-previous))
-  :init
-  (add-to-list 'company-backends 'company-jedi)
-  (add-to-list 'company-backends 'company-robe)
-  (progn
-    (set-face-attribute 'company-tooltip nil
-                        :foreground "black" :background "lightgrey")
-    (set-face-attribute 'company-tooltip-common nil
-                        :foreground "black" :background "lightgrey")
-    (set-face-attribute 'company-tooltip-common-selection nil
-                        :foreground "white" :background "steelblue")
-    (set-face-attribute 'company-tooltip-selection nil
-                        :foreground "black" :background "steelblue")
-    (set-face-attribute 'company-preview-common nil
-                        :background nil :foreground "lightgrey" :underline t)
-    (set-face-attribute 'company-scrollbar-fg nil
-                        :background "orange")
-    (set-face-attribute 'company-scrollbar-bg nil
-                        :background "gray40"))
   :config
   (setq company-idle-delay 0)
   (setq company-minimum-prefix-length 2)
   (setq company-selection-wrap-around t))
+
+(use-package company-jedi
+  :after company
+  :config
+  (push 'company-jedi company-backends))
+
+(use-package company-robe
+  :after company
+  :config
+  (push 'company-robe company-backends))
 
 (provide 'config-company)
 ;;; conf-company.el ends here
